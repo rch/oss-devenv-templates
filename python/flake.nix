4f086b9df2899f443e70c72d1f35af6f2e524637
Dileep Kishore <k.dileep1994@gmail.com>
2024-03-24 21:57:59 -0400

    feat: update python devenv to use devenv inputs

--- python/flake.nix
+++ python/flake.nix
@@ -3,70 +3,78 @@
   description = "Description of project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
+      default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
+            packages = with pkgs; [
+              stdenv.cc.cc.lib # required by Jupyter
+              zlib
+              glibc
+              python311Packages.pip
+              ruff
+              nodePackages.pyright
+            ];
 
-      devShells = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [{
-              packages = with pkgs; [
-                stdenv.cc.cc.lib # required by Jupyter
-                zlib
-                glibc
-                python311Packages.pip
-                ruff
-                nodePackages.pyright
-              ];
+            env = {GREET = "󱄅 Nix";};
 
-              env = { GREET = "󱄅 Nix"; };
+            scripts.hello.exec = "echo $GREET";
 
-              scripts.hello.exec = "echo $GREET";
+            enterShell = ''
+              hello
+            '';
 
-              enterShell = ''
-                hello
-              '';
-
-              languages.python = {
-                package = pkgs.python311;
+            languages.python = {
+              package = pkgs.python311;
+              enable = true;
+              poetry = {
                 enable = true;
-                poetry = {
-                  enable = true;
-                  activate.enable = true;
-                  install.enable = true;
-                  install.installRootPackage = true;
-                  install.allExtras = true;
-                };
+                activate.enable = true;
+                install.enable = true;
+                install.installRootPackage = true;
+                install.allExtras = true;
               };
+            };
 
-              pre-commit.hooks = {
-                ruff.enable = true;
-                pyright.enable = true;
-                shellcheck.enable = true;
-                markdownlint.enable = true;
-                alejandra.enable = true;
-                editorconfig-checker.enable = true;
-              };
+            pre-commit.hooks = {
+              ruff.enable = true;
+              shellcheck.enable = true;
+              markdownlint.enable = true;
+              alejandra.enable = true;
+              editorconfig-checker.enable = true;
+            };
 
-              dotenv.enable = true;
-            }];
-          };
-        });
-    };
+            dotenv.enable = true;
+          }
+        ];
+      };
+    });
+  };
 }
