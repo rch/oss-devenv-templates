1a6cd589950c9b530e4e2e209fd06a0140fcec7c
Dileep Kishore <k.dileep1994@gmail.com>
2024-11-16 06:04:54 -0500

    feat: add julia template

---
+++ julia/flake.nix
@@ -0,0 +1,73 @@
+{
+  # TODO: Add a description
+  description = "Description of project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
+            packages = with pkgs; [
+              stdenv.cc.cc.lib # required by Jupyter
+              zlib
+              glibc
+              python312Packages.pip
+              ruff
+              pyright
+              just
+            ];
+
+            env = {GREET = "󱄅 Nix";};
+
+            scripts.hello.exec = "echo $GREET";
+
+            languages.julia = {
+              enable = true;
+            };
+
+            pre-commit.hooks = {
+              juliaformatter.enable = true;
+              shellcheck.enable = true;
+              markdownlint.enable = true;
+              alejandra.enable = true;
+              editorconfig-checker.enable = true;
+            };
+
+            dotenv.enable = true;
+
+            env.LD_LIBRARY_PATH = with pkgs;
+              lib.makeLibraryPath [
+                stdenv.cc.cc.lib
+                zlib
+              ];
+          }
+        ];
+      };
+    });
+  };
+}
