1324dfa4b3f644ee088731a129fd13191e523f2d
Dileep Kishore <k.dileep1994@gmail.com>
2024-11-16 06:07:23 -0500

    fix: add quarto and julia templates to flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,22 @@
 {
-  description =
-    "A collection of development environment configurations based on Nix flakes";
+  description = "A collection of development environment configurations based on Nix flakes";
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
     templates = {
       python = {
         path = ./python;
         description = "Python development environment";
+      };
+      quarto = {
+        path = ./quarto;
+        description = "Quarto notes environment";
+      };
+      julia = {
+        path = ./julia;
+        description = "Julia development environment";
       };
     };
   };
